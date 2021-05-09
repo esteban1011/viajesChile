ad179edff29c1914a7f09c6c97c7b2cf4de7236f
esteban1011 <esteban.m.g4@gmail.com>
2021-05-09 19:31:08 -0400

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+document.write("")
+
+$(document).ready(function(){
+    $("#oculto1").click(function(){
+        $(this).hide(700);
+    });
+    });
+
+    $(document).ready(function(){
+        $("#oculto2").click(function(){
+            $(this).hide(700);
+        });
+        });
+
+        $(document).ready(function(){
+            $("#oculto3").click(function(){
+                $(this).hide(700);
+            });
+            });
+
